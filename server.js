9e0959469123586749a18abf837c55256114a883
codingherd <codingherd@gmail.com>
2018-10-25 18:17:28 +0530

    port change

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/angular-ci/index.html'));
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 app.set('port', port);
 
 const server = http.createServer(app);
